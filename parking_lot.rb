45d710a5b6180d724a005871353c5c552b431a50
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-02-09 15:30:50 -0700

    Define list cars and call methods in parking_lot

--- parking_lot.rb
+++ parking_lot.rb
@@ -1,11 +1,20 @@
-require_relative "car.rb"
+require_relative "./car.rb"
 
 class ParkingLot
   def initialize (filename)
-    cars_hash = {}
+    @cars_hash = {}
     cars = File.open(filename, "r")
-    cars_array = cars.read.split("\n\n\n").map{|words| words.split}.map{|car| cars_hash[car[0]] = car[1]}
-    puts cars_hash
+    cars_array = cars.read.split("\n\n\n").map{|words| words.split}.map{|car| @cars_hash[car[0]] = car[1]}
+    puts @cars_hash
+  end 
+
+  def list_cars
+    @cars_hash.each do |key, value|
+      key = Car.new(key, value)
+      key.what_car
+    end
   end
-  
 end
+
+newLot = ParkingLot.new("cars_on_lot.txt")
+newLot.list_cars
