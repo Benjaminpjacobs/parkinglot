ad233fd08dd8eb448fcfc8426207df41055fd806
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-02-09 15:25:01 -0700

    Define initialize method for Parking Lot

--- parking_lot.rb
+++ parking_lot.rb
@@ -2,8 +2,10 @@ require_relative "car.rb"
 
 class ParkingLot
   def initialize (filename)
-    cars = File.read(filename, "r")
-    cars_array = cars.read.split("\n\n\n").map{|words| words.split}
+    cars_hash = {}
+    cars = File.open(filename, "r")
+    cars_array = cars.read.split("\n\n\n").map{|words| words.split}.map{|car| cars_hash[car[0]] = car[1]}
+    puts cars_hash
   end
   
 end
